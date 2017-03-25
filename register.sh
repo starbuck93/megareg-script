c3200c47640b04e6a5f9d6f9a43a0129646996d2
Adam Starbuck <me.starbuck@gmail.com>
2017-03-25 16:21:29 +0000

    added README and added a few essential files. and edited the paths of register.sh

--- register.sh
+++ register.sh
@@ -4,28 +4,28 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-source /home/user/.account
+source .account
 
 echo "Registering storage$1@$EMAILDOMAIN"
 CONFIRM="$(megareg --scripted --register --email storage"$1"@$EMAILDOMAIN --name user name --password $PASSWORD)"
 #echo "megareg run"
-echo "$CONFIRM" >> /home/user/megarc/links
-cp ~/megarc/storageTest ~/megarc/storage"$1"
-sed -i "s/TEST/$1/g" ~/megarc/storage"$1"
+echo "$CONFIRM" >> megarc/links
+cp megarc/storageTest megarc/storage"$1"
+sed -i "s/TEST/$1/g" megarc/storage"$1"
 sleep 1
-LINK="$(php /home/user/getLink.php $1)"
+LINK="$(php getLink.php $1)"
 while [ "$LINK" = "0" ]
 do
 	echo "Not yet"
 	sleep 1
-	LINK="$(php /home/user/getLink.php)"
+	LINK="$(php getLink.php)"
 	echo ">$LINK<"
 done
 
 link3=${CONFIRM/@LINK@/\'"$LINK"\'}
 eval $link3
 #echo "<< Welcome email"
-echo $link3 >> /home/user/megarc/links
+echo $link3 >> megarc/links
 #echo "Press enter to clear the mailbox or CTRL+C to stop"
 #read asdf
 sleep 1
