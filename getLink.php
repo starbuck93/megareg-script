d0e5a3cc16ad1dbb3553a743dead76a0c8b7ab36
Adam Starbuck <me.starbuck@gmail.com>
2017-06-01 21:00:50 +0000

    Modified a bunch of things!

--- getLink.php
+++ getLink.php
@@ -1,6 +1,6 @@
 <?php
 
-$mailbox = "/var/mail/user";
+$mailbox = "/var/mail/ubuntu";
 $findString = "https://mega.nz/#confirm";
 
 $linkPos = strpos(file_get_contents($mailbox),$findString);
@@ -9,12 +9,12 @@ else {
 	$endlinePos = strpos(file_get_contents($mailbox),"\n",$linkPos);
 	$link = substr(file_get_contents($mailbox),$linkPos,$endlinePos-$linkPos);
 	echo $link;
-	$filename = "/home/user/megarc/links";
+	$filename = "/home/ubuntu/megareg-script/megarc/links";
 
 	//DATE=`date +%Y-%m-%d.%H:%M:%S`;
 
 	file_put_contents($filename,$link . " <<>> $argv[1]\n", FILE_APPEND);
-	exec("cp /var/mail/user /home/user/Mbox/mbox`date +%Y-%m-%d.%H:%M:%S`");
-	//exec(" > /var/mail/user");
+	exec("cp /var/mail/ubuntu /home/ubuntu/Mbox/mbox`date +%Y-%m-%d.%H:%M:%S`");
+	//exec(" > /var/mail/ubuntu");
 }
 ?>
